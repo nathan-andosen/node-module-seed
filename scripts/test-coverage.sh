6f2e5704d91116d85ed778d915b2b8f4d6152e1a
Nathan Anderson <nathan.andosen@gmail.com>
2019-08-11 14:54:48 +1000

    Fix test coverage not working

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nyc --check-coverage node ./scripts/unit-tests.js
+nyc --exclude-after-remap false --check-coverage node ./scripts/unit-tests.js
 
 node ./scripts/update-readme-with-shield-badge.js
